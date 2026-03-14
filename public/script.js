2c8a7167a459e706532ed1baf575cc42e4b5add0
mahendra <pratapm8@gmail.com>
2026-03-14 09:05:43 +0530

    all

--- public/script.js
+++ public/script.js
@@ -77,14 +77,40 @@ let isVideoOn = false;
 const servers = {
     iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' }
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
     ]
 };
 
 // 1. Get access to microphone
 async function getMedia() {
     try {
-        localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+        localStream = await navigator.mediaDevices.getUserMedia({ 
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+                autoGainControl: true,
+                sampleRate: 48000
+            }, 
+            video: false 
+        });
     } catch (err) {
         console.error('Error accessing media devices.', err);
         statusMsg.innerHTML = '<span style="color:#e74c3c">Microphone access required for audio calls!</span>';
